5f3bc111b448c58f8e2ca1d08e9f3a4cbde55f03
Ivana Pavlovic <ivanapavlovic314@users.noreply.github.com>
2022-02-15 17:00:09 +0100

    2nd commit

---
+++ src/test/java/com/example/JavaHelloWorldTest.java
@@ -0,0 +1,11 @@
+package com.example;
+
+import org.junit.jupiter.api.Test;
+
+class JavaHelloWorldTest {
+
+    @Test
+    void getHello() {
+        System.out.println(new JavaHelloWorld().getHello());
+    }
+}
